0a461e5d4ac7d92a0eb43dbf065b330737dd11ab
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-06-07 17:36:04 +0300

    add Routs and Rout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
+import { Home } from 'pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import { Movies } from 'pages/Movies';
+import { MovieDetails } from 'pages/MovieDetails';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    ></div>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route index element={<Home />} />
+      <Route path="movies" element={<Movies />} />
+      <Route path="" element={<MovieDetails />}></Route>
+    </Routes>
+    // <div
+    //   style={{
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101',
+    //   }}
+    // ></div>
   );
 };
